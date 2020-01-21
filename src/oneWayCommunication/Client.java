28848dd87b78521999449e3c99859b51e3b6ce6c
Your Name <you@example.com>
2020-01-21 21:18:29 +0600

    One way communication done

--- src/oneWayCommunication/Client.java
+++ src/oneWayCommunication/Client.java
@@ -1,7 +1,26 @@
 package oneWayCommunication;
 
-public class Client {
-    public static void main(String[] args) {
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.Scanner;
 
+public class Client {
+    public static void main(String[] args) throws IOException {
+
+        //setting up a new client socket
+        Socket s = new Socket("localhost", 4999);
+
+        //taking user input
+        String str;
+        Scanner scanner = new Scanner(System.in);
+        str = scanner.nextLine();
+
+        //sending data to server
+        DataOutputStream outputStream = new DataOutputStream(s.getOutputStream());
+        outputStream.writeUTF(str);
+        outputStream.flush();
+
+        s.close();
     }
 }
--- src/oneWayCommunication/Server.java
+++ src/oneWayCommunication/Server.java
@@ -1,7 +1,31 @@
 package oneWayCommunication;
 
+import java.awt.image.DataBufferInt;
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
 public class Server {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
+
+
+        ServerSocket ss = new ServerSocket(4999); //setting up server socket
+        System.out.println("Server is waiting...");
+        Socket s = ss.accept(); //establishing the connection with client
+        System.out.println("Client has connected.");
+
+        //receiving the data sent by the user
+        String str;
+        DataInputStream input = new DataInputStream(s.getInputStream());
+        str = input.readUTF();
+
+        //printing the received data
+        System.out.println("Client: " + str);
+
+        input.close();
+        s.close();
+        ss.close();
 
     }
 }
